192757e982eabd3ddbbf1bccb778fb60eb8363f5
rpedsel <rpedsel@gmail.com>
2017-04-04 23:08:09 -0700

    into 28*28 blocks

--- text_rec.py
+++ text_rec.py
@@ -31,15 +31,17 @@ def union(rect1, rect2):
     h = max(rect1.y + rect1.h, rect2.y + rect2.h) - y
     return Rect(x, y, w, h)
 
-img = cv2.imread('bw_image.jpg')
-h_img, w_img,_ = img.shape
+img = cv2.imread('mnist.jpg', cv2.IMREAD_GRAYSCALE)
+h_img, w_img = img.shape
 size = h_img*w_img
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Converting to GrayScale
-vis = img.copy()
-ret,thresh = cv2.threshold(gray,127,255,0)
+#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Converting to GrayScale
+#vis = img.copy()
+_,thresh = cv2.threshold(img,127,255,0)
 _,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 #cv2.drawContours(vis, contours, -1, (0,255,0), 1)
+
+#(thresh, im_bw) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
 rectList = []
 for cnt in contours:
@@ -60,9 +62,31 @@ while(count > 0):
     count -= 1
     rectList = resRect
 
-for r in resRect:
-    cv2.rectangle(vis,(r.x,r.y),(r.x+r.w,r.y+r.h),(0,0,255),1)
+#for r in resRect:
+#    cv2.rectangle(img,(r.x,r.y),(r.x+r.w,r.y+r.h),(0,0,255),1)
 
-cv2.imwrite('output.jpg', vis)
+maxwh,maxw = max(resRect[4].w,resRect[4].h), resRect[4].w > resRect[4].h 
+crop=cv2.resize(img[resRect[4].y:resRect[4].y+resRect[4].h,resRect[4].x:resRect[4].x+resRect[4].w],(0,0),fx=22/maxwh, fy=22/maxwh)
+
+(_, crop) = cv2.threshold(crop, 128,255,cv2.THRESH_BINARY|cv2.THRESH_OTSU)
+crop = 1-crop/255
+cv2.imwrite("crop.jpg", crop)
+
+mat = np.zeros((28,28))
+if maxw:
+    print(crop.shape)
+    starty = int(np.ceil((28-crop.shape[0])/2))
+    print(starty)
+    mat[starty:startx+crop.shape[0], 3:3+crop.shape[1]] = crop
+else:
+    print(crop.shape)
+    startx = int(np.ceil((28-crop.shape[1])/2))
+    print(startx)
+    mat[3:3+crop.shape[0], startx:startx+crop.shape[1]] = crop
+print(mat)
+print(mat.shape)
+
+
+cv2.imwrite('output.jpg',mat*255)
 
 
